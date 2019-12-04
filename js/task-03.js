98bac63c57bd2400281a6aa574cddcfbaddb168a
Alexey <valnistuy@gmail.com>
2019-12-04 20:07:23 +0200

    added task-03

---
+++ js/task-03.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import users from "./users.js";
+
+// Получить массив имен пользователей по полу (поле gender).
+
+const getUsersWithGender = (users, gender) =>
+  users.filter(user => user.gender === gender).map(user => user.name);
+// твой код
+
+console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
