b3d3a9560c179165c1bd3357a054e8eea2cef855
Alexey <valnistuy@gmail.com>
2019-12-04 19:37:53 +0200

    create hw-js-06 and add task-01

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import users from "./users.js";
+
+// Получить массив имен всех пользователей (поле name).
+
+const getUserNames = users => {
+  // твой код
+  return users.map(user => {
+    return user.name;
+  });
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
