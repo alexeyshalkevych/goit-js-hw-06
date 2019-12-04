9356eab1c7282ffef262fa15b73f7131153c45b1
Alexey <valnistuy@gmail.com>
2019-12-04 19:52:46 +0200

    add task-02 and change task-01

--- js/task-01.js
+++ js/task-01.js
@@ -4,12 +4,8 @@ import users from "./users.js";
 
 // Получить массив имен всех пользователей (поле name).
 
-const getUserNames = users => {
-  // твой код
-  return users.map(user => {
-    return user.name;
-  });
-};
+const getUserNames = users => users.map(user => user.name);
+// твой код
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
