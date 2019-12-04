1ab019702efe712f4aec5f0b78cc412c9d0d2df4
Alexey <valnistuy@gmail.com>
2019-12-04 20:15:13 +0200

    added task-05

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import users from "./users.js";
+
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) =>
+  users.find(user => user.email === email);
+// твой код
+
+console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
