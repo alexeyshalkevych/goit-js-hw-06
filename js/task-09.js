254c5b48376bc223fd063b1ffc1559f21f07fcc2
Alexey <valnistuy@gmail.com>
2019-12-04 21:03:57 +0200

    added task-09

---
+++ js/task-09.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import users from "./users.js";
+
+// Массив имен (поле name) людей, отсортированных в зависимости от количества
+// их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users =>
+  users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(user => user.name);
+// твой код
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
